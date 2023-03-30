91754953ba3697951840d5e9115629b2fb84e697
John Brouwers <john.brouwers@outlook.com>
2023-03-30 11:31:26 +0200

    nullable statussen in viewmodel

--- TakenlijstManager/Models/CreateTaakViewModel.cs
+++ TakenlijstManager/Models/CreateTaakViewModel.cs
@@ -24,6 +24,6 @@ namespace TakenlijstManager.Models
         [DisplayName("Status")]
         public int? StatusId { get; set; }
 
-        public SelectList Statussen { get; set; }
+        public SelectList? Statussen { get; set; }
     }
 }
